9680c593993272d125aa71ce5c7b1cc8599d9649
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-02-06 00:13:41 -0600

    adding Database url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV:process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/innerengineering',
+    DATABASE_URL:'postgres://fspqgcdulllhtv:e3d675bc1050701d4f8d9a50ea8838e0987ee2e332d5f7614d6456af29e7c58b@ec2-34-230-167-186.compute-1.amazonaws.com:5432/d2fje472pkkbpp?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
+    //DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/innerengineering',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/innerengineering-test'
 }
